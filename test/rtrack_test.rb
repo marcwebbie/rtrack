6529bfcdcf6f30b9e69db710e5be8e995e5a3e4b
marcwebbie <marcwebbie@gmail.com>
2014-05-14 20:33:20 -0300

    Refactored strings in test to be single quoted when there is no interpolation

--- test/rtrack_test.rb
+++ test/rtrack_test.rb
@@ -6,34 +6,34 @@ require 'rtrack/transcoder'
 
 class Audiorack_Tests < Test::Unit::TestCase
   def setup
-    @track = AudioTrack.from_file "test/data/test.mp3"
+    @track = AudioTrack.from_file 'test/data/test.mp3'
   end
 
   def test_instantiate_audio_track_from_file
-    test_track = AudioTrack.from_file "test/data/test.mp3"
+    test_track = AudioTrack.from_file 'test/data/test.mp3'
     assert_not_nil test_track
     assert_equal test_track.class, AudioTrack
   end
 
   def test_from_file_throw_exception_when_wrong_infile_path_is_given
     assert_raises(StandardError) do
-      test_track = AudioTrack.from_file "test/data/wrong_path.mp3"
+      test_track = AudioTrack.from_file 'test/data/wrong_path.mp3'
       assert_not_nil test_track
     end
   end
 
   def test_audio_track_has_a_not_nil_wavefile_from_given_infile
-    test_track = AudioTrack.from_file "test/data/test.mp3"
+    test_track = AudioTrack.from_file 'test/data/test.mp3'
     assert_not_nil test_track.wavefile
   end
 
   def test_from_file_returns_audio_track_with_expected_duration
-    test_track = AudioTrack.from_file "test/data/test.mp3"
+    test_track = AudioTrack.from_file 'test/data/test.mp3'
     expected_duration = 10009
     assert_respond_to test_track, "duration"
     assert_equal expected_duration, test_track.duration
 
-    test_track = AudioTrack.from_file "test/data/test2.mp3"
+    test_track = AudioTrack.from_file 'test/data/test2.mp3'
     expected_duration = 5016
     assert_respond_to test_track, "duration"
     assert_equal expected_duration, test_track.duration
@@ -49,9 +49,9 @@ class Audiorack_Tests < Test::Unit::TestCase
     assert_respond_to @track, "[]"
     expected_seconds = Random.rand(1..10)
 
-    slice = AudioTrack.from_file("test/data/test.mp3")[0..expected_seconds]
+    slice = AudioTrack.from_file('test/data/test.mp3')[0..expected_seconds]
     assert_equal slice.seconds, expected_seconds
-    slice = AudioTrack.from_file("test/data/test.mp3")[0...expected_seconds]
+    slice = AudioTrack.from_file('test/data/test.mp3')[0...expected_seconds]
     assert_equal slice.seconds, expected_seconds-1
   end
 
@@ -66,8 +66,8 @@ class Audiorack_Tests < Test::Unit::TestCase
   end
 
   def test_concatenate_two_audio_tracks_has_duration_equal_to_sum_of_durations
-    track_one = AudioTrack.from_file("test/data/test.mp3")
-    track_two = AudioTrack.from_file("test/data/test2.mp3")
+    track_one = AudioTrack.from_file('test/data/test.mp3')
+    track_two = AudioTrack.from_file('test/data/test2.mp3')
 
     assert_respond_to track_one, "+"
     track_three = track_one + track_two
